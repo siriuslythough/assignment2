b3d56158bb2eb134194c3c939bd999631dc6d5b5
siriuslythough <135163094+siriuslythough@users.noreply.github.com>
2023-06-16 07:26:08 +0530

    Add files via upload

---
+++ assgn 2 problem a.R
@@ -0,0 +1,7 @@
+#problema
+data(iris)
+boxplot(iris)
+Sepal.Length=iris[1:150,1]
+Petal.Length=iris[1:150,2]
+plot(Petal.Length,Sepal.Length)
+#one of the conclusions is that most varied sample in sepal length is seen for petal length 3.0 units 
